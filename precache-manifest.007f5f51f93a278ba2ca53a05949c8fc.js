9ba1e916ce9ebe7f31fe0c0e3231035205cc36f0
Sahil Sutaria <saahil.sutaria@gmail.com>
2020-08-27 13:01:22 -0700

    Updates

---
+++ precache-manifest.007f5f51f93a278ba2ca53a05949c8fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf497297e4d42cfcf1e5cfc5558de5e6",
+    "url": "/Reduxstagram/index.html"
+  },
+  {
+    "revision": "1d3a3c310686166cd132",
+    "url": "/Reduxstagram/static/css/main.3196c495.chunk.css"
+  },
+  {
+    "revision": "a54faf53c880aa0c6d6f",
+    "url": "/Reduxstagram/static/js/2.bd9b76fb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Reduxstagram/static/js/2.bd9b76fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d3a3c310686166cd132",
+    "url": "/Reduxstagram/static/js/main.a903b84d.chunk.js"
+  },
+  {
+    "revision": "61e3d3634cb2a4508729",
+    "url": "/Reduxstagram/static/js/runtime-main.5bfaf37a.js"
+  }
+]);
